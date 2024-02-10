fd4248e64ddd6da831345cafe9452cf9aa1f68e0
Shreya Chaudhari <143866457+shreya-chaudhari-512@users.noreply.github.com>
2024-02-10 12:07:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Signup from "./pages/Signup";
+import Model from "./pages/Model";
+
+function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/signup":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/model":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/Signup" element={<Signup />} />
+      <Route path="/Model" element={<Model />} />
+    </Routes>
+  );
+}
+export default App;
